ff6a686802280c682dc78a11fbcb00d0e32047a4
Amadu Sadjo Camara <amaduami23@gmail.com>
2024-12-25 16:17:25 +0530

    react first project

---
+++ react/003-react/pizza-list01/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+      <imge src="logo192.png" alt="logo react"/>
+    </div>
+  );
+}
+
+export default App;
