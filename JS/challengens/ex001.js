9a7a6d87d8b6770e3b85526971f63aa9f97ef803
Amadu Sadjo Camara <amaduami23@gmail.com>
2025-01-02 16:43:16 +0530

    challenge 001 solved

---
+++ JS/challengens/ex001.js
@@ -0,0 +1,10 @@
+//Reverse a String: Write a function that takes a string and returns it in reverse.
+
+const myString = function (s) {
+  const reverName = s.split("").reverse().join("");
+  return reverName;
+};
+
+console.log(myString(""));
+
+//const reverName = nameScl.split(""),reverse();
